6d31072b91ef04d963c4a51a6836155786d26e6e
Eddie Lee <edl56@cornell.edu>
2016-08-11 19:37:31 -0400

    load()

---
+++ utils.py
@@ -0,0 +1,48 @@
+# 2016-08-11
+
+import pandas as pd
+import numpy as np
+
+def load(fname,includeDisplacement=False):
+    """
+    Load data from BVD file.
+    2016-08-11
+
+    Params:
+    -------
+    fname (str)
+        Name of file to load
+    includeDisplacement (bool=False)
+        If displacement data is included for everything including root.
+    """
+    # Find the line where data starts and get skeleton parts.
+    from itertools import chain
+    bodyParts = []
+    with open(fname) as f:
+        i = 0
+        ln = f.readline()
+        while not 'ROOT' in ln:
+            ln = f.readline()
+            i += 1
+        bodyParts.append( ''.join(a for a in ln.split(' ')[1] if a.isalnum()) )
+        while not 'Frames' in ln:
+            if 'JOINT' in ln:
+                ix = ln.find('JOINT')
+                bodyParts.append( ''.join(a for a in ln[ix:].split(' ')[1] if a.isalnum()) )
+            ln = f.readline()
+            i += 1
+    df = pd.read_csv(fname,skiprows=i+2,delimiter=' ',header=None)
+    df = df.iloc[:,:-1]  # remove bad last col
+    
+    if includeDisplacement:
+        df.columns = pd.MultiIndex.from_arrays([list(chain.from_iterable([[b]*6 for b in bodyParts])),
+                                            ['xx','yy','zz','y','x','z']*len(bodyParts)])
+    else:
+        df.columns = pd.MultiIndex.from_arrays([[bodyParts[0]]*6 + 
+                                                 list(chain.from_iterable([[b]*3 for b in bodyParts[1:]])),
+                                            ['xx','yy','zz']+['y','x','z']*len(bodyParts)])
+    
+    # Only keep entries that change at all.
+    df = df.iloc[:,np.diff(df,axis=0).sum(0)!=0] 
+    return df 
+
