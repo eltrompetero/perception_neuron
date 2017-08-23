c51b75c1549a391013d385001dff06a4812cad46
Eddie Lee <edl56@cornell.edu>
2017-08-23 16:27:19 -0400

    Added cwt_coherence()

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ import numpy as np
 from numpy import sin,cos,pi
 import pandas as pd
 from scipy.interpolate import LSQUnivariateSpline,UnivariateSpline,interp1d
-import entropy.entropy as info
+import entropy as info
 from scipy.signal import fftconvolve
 from misc.utils import unique_rows
 import load
@@ -78,6 +78,22 @@ class MultiUnivariateSpline(object):
 # ===================== #
 # Function definitions. #
 # ===================== #
+def cwt_coherence(x,y,nskip,scale=np.arange(10,100),**kwargs):
+    # Use the wavelet transform to measure coherence.
+    xcwt,f = pywt.cwt(x,scale,'cgau1',sampling_period=1/60,**kwargs)
+    ycwt,f = pywt.cwt(y,scale,'cgau1',sampling_period=1/60,**kwargs)
+    
+    # Get indices of points with some overlap.
+    selectix = np.arange(nskip,len(subv),nskip,dtype=int)
+
+    Psub = ( np.abs(xcwt[:,selectix])**2 ).mean(-1)
+    Pav = ( np.abs(ycwt[:,selectix])**2 ).mean(-1)
+    Pcross = ( np.abs(xcwt[:,selectix]*ycwt[:,selectix].conjugate()) ).mean(-1)
+
+    coh = np.abs(Pcross)**2/Psub/Pav
+
+    return -np.trapz(coh[f<10],x=f[f<10])/(f[f<10].max()-f[f<10].min())
+
 def max_coh_time_shift(subv,temv,
                        dtgrid=np.linspace(0,1,100),
                        mx_freq=10,
