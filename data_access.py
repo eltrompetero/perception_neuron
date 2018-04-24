f78fac25b964fde89db88abc0f539195ac931c26
Eddie Lee <edl56@cornell.edu>
2018-04-24 19:46:48 -0400

    Added subject_settings_v3_6().

--- data_access.py
+++ data_access.py
@@ -383,6 +383,65 @@ def subject_settings_v3_5(index,hand,return_list=True):
         return settings['person'],dr,rotAngle,reverse,usable
     return settings,dr
 
+def subject_settings_v3_6(index,hand,return_list=True):
+    """
+    Subject info for experiment v3.3. Twoples refer to the left and right subject hands.
+    2018-04-22
+
+    Parameters
+    ----------
+    index : int
+    hand : str
+        Subject hand used.
+    return_list : bool,True
+
+    Returns
+    -------
+    settings : dict
+    dr : str
+    """
+    settings = [{'person':'Subject01_3_6',
+                 'trials':['avatar'],
+                 'reverse':[False,True],
+                 'usable':[True,True]},
+                {'person':'Subject02_3_6',
+                 'trials':['avatar'],
+                 'reverse':[True,False],
+                 'usable':[True,True]},
+                {'person':'Subject03_3_6',
+                 'trials':['avatar'],
+                 'reverse':[True,False],
+                 'usable':[True,True]},
+                {'person':'Subject04_3_6',
+                 'trials':['avatar'],
+                 'reverse':[False,True],
+                 'usable':[True,True]},
+                {'person':'Subject05_3_6',
+                 'trials':['avatar'],
+                 'reverse':[False,True],
+                 'usable':[True,False]},
+                {'person':'Subject06_3_6',
+                 'trials':['avatar'],
+                 'reverse':[False,True],
+                 'usable':[True,True]},
+                {'person':'Subject07_3_6',
+                 'trials':['avatar'],
+                 'reverse':[True,False],
+                 'usable':[True,True]}
+                ][index]
+    dr = '../data/UE4_Experiments/%s/%s'%(settings['person'],hand)
+    try:
+        rotAngle = pickle.load(open('%s/%s'%(dr,'gpr.p'),'rb'))['rotAngle']
+    except IOError:
+        rotAngle=np.nan
+    reverse=settings['reverse'][0] if hand=='left' else settings['reverse'][1]
+    usable=settings['usable'][0] if hand=='left' else settings['usable'][1]
+
+    if return_list:
+        return settings['person'],dr,rotAngle,reverse,usable
+    return settings,dr
+
+
 
 
 # ------------------ #
